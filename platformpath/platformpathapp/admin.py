135ea8ad3dd7798bee0a9da2ee80d43627d4522e
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-03 19:58:07 -0400

    Registered models to be modified in admin interface.

--- platformpath/platformpathapp/admin.py
+++ platformpath/platformpathapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Station, Line, Node, Edge
 
 # Register your models here.
+admin.site.register(Station)
+admin.site.register(Line)
+admin.site.register(Node)
+admin.site.register(Edge)
