8601e4ec12c35a9c1160b4cebad94bd2300f0bdb
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-03 19:51:30 -0400

    Created Line, Station, Node, and Edge model for the django database to store station info.

--- platformpath/platformpathapp/models.py
+++ platformpath/platformpathapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+class Line(models.Model):
+    name = models.CharField(max_length=10)
+
+class Station(models.Model):
+    name = models.CharField(max_length=200)
+    diagram_path = models.CharField(max_length=100)
+    lines = models.ManyToManyField(Line, blank=True)
+
+class Node(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    node_type = models.CharField(max_length=20)
+    label = models.CharField(max_length=200)
+    svg_id = models.CharField(max_length=100)
+    layer = models.CharField(max_length=100)
+
+class Edge(models.Model):
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    from_node = models.ForeignKey(Node, related_name='edges_from',
+                                             on_delete=models.CASCADE)
+    to_node = models.ForeignKey(Node, related_name='edges_to',
+                                             on_delete=models.CASCADE)
+    instruction_forward = models.CharField(max_length=300)
+    instruction_backward = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
