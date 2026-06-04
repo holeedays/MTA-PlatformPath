4dfe0b111523e84132b98833e7d8eaf6ea3ba7fa
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-03 20:25:08 -0400

    Added __str__ methods to each model class to facilitate Django admin interface of database and debugging.

--- platformpath/platformpathapp/models.py
+++ platformpath/platformpathapp/models.py
@@ -3,10 +3,16 @@ from django.db import models
 class Line(models.Model):
     name = models.CharField(max_length=10)
 
+    def __str__(self):
+        return self.name
+
 class Station(models.Model):
     name = models.CharField(max_length=200)
     diagram_path = models.CharField(max_length=100)
     lines = models.ManyToManyField(Line, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Node(models.Model):
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
@@ -14,6 +20,9 @@ class Node(models.Model):
     label = models.CharField(max_length=200)
     svg_id = models.CharField(max_length=100)
     layer = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.station.name} - {self.label}"
 
 class Edge(models.Model):
     station = models.ForeignKey(Station, on_delete=models.CASCADE)
@@ -24,3 +33,6 @@ class Edge(models.Model):
     instruction_forward = models.CharField(max_length=300)
     instruction_backward = models.CharField(max_length=300)
     is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.from_node.label} - {self.to_node.label}"
