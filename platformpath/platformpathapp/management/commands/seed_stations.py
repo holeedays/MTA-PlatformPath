b252b1b05e15315618b61f2467237dbf26809c86
IvanYeung0610 <ivanyeung0610@gmail.com>
2026-06-03 21:25:23 -0400

    Created seeding script and a directory to organize information about different stations for the seeding process.

--- platformpath/platformpathapp/management/commands/seed_stations.py
+++ platformpath/platformpathapp/management/commands/seed_stations.py
@@ -1,6 +1,21 @@
 from django.core.management import BaseCommand
 from platformpathapp.models import Station, Line, Node, Edge
 
+# Import individual stations
+from platformpathapp.management.stations import bay_50_st
+
 class Command(BaseCommand):
     help = 'Seed initial station data'
 
+    def handle(self, *args, **kwargs):
+        self.stdout.write("Clearing old station data....")
+
+        Station.objects.all().delete()
+        Line.objects.all().delete()
+
+        self.stdout.write("Starting database seed...\n")
+
+        # Seed each station
+        bay_50_st.seed(self.stdout, self.style)
+
+        self.stdout.write(self.style.SUCCESS("\nAll station data seeded successfully!"))
