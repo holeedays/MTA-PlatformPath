9d75d52beb521bc31fb3578dfa31610abf313c45
Edmond Huang <weighuang12@gmail.com>
2026-06-18 00:38:28 -0400

    Reconfigured stations selection page logic as well as the static template it uses + added a custom css page for the static template

--- platformpath/platformpathapp/static/platformpathapp/js/z_stations_selection_page.js
+++ platformpath/platformpathapp/static/platformpathapp/js/z_stations_selection_page.js
@@ -28,64 +28,68 @@ export class StationsSelectionPage {
         // fetching data for each line
         const stationsData = await DataFetch.fetchStations(lines, URLS.STATIONS_FETCH_API);
         try {
-            const stationsSelectionContainer = document.getElementById("stations_selection_container");
-            if (stationsSelectionContainer === null)
-                throw Error("The station selection container div doesn't exist");
-            // iterating through each line
-            for (const line in stationsData) {
-                // create a parent div to house all the forms we'll be creating
-                const formContainer = document.createElement("div");
-                formContainer.innerHTML = line;
-                // this create a select dropdown form while allowing user typing as well     
-                const datalistForm = document.createElement("datalist");
-                datalistForm.setAttribute("id", `dropdown_for_${line}`);
-                const inputForm = document.createElement("input");
-                inputForm.setAttribute("list", `dropdown_for_${line}`);
-                // save the previous value to remove query params (if the user were to remove them)
-                let previous_value = "";
-                inputForm.addEventListener("change", () => {
-                    // if an option was selected or form was filled
-                    if (inputForm.value !== "") {
-                        // check if the value put in the input form exists within the datalist
-                        // this is only used in the cases where the user types a value instead of checking an option
-                        let isValidValue = false;
-                        for (const option of datalistForm.options) {
-                            if (inputForm.value === option.value) {
-                                URLHandler.addQueryParameter("selected_station", inputForm.value);
-                                isValidValue = true;
-                            }
-                        }
-                        if (!isValidValue)
-                            inputForm.value = "";
-                    }
-                    else { // if inputForm is empty
-                        //check if query param exists
-                        const allQueryParams = URLHandler.getQueryParameters();
-                        if (allQueryParams["selected_station"] !== undefined) {
-                            for (const queryParam of allQueryParams["selected_station"]) {
-                                if (queryParam === previous_value) {
-                                    URLHandler.removeQueryParameter("selected_station", previous_value);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                    previous_value = inputForm.value;
-                });
-                // create all the dropdown options of available stations
-                stationsData[line]?.forEach((stationObj) => {
-                    const option = document.createElement("option");
-                    option.setAttribute("value", stationObj.name);
-                    option.textContent = stationObj.name;
-                    datalistForm.appendChild(option);
-                });
-                // configure all these items so they are grouped up properly in the html
-                formContainer.append(datalistForm, inputForm);
-                stationsSelectionContainer.appendChild(formContainer);
-            }
+            // initiate the logic for the input forms of the lines the users selected
+            this.initSelectedLinesInputForms(stationsData);
+            // initiate the logic for the input forms of the lines the users did not select (remove them basically)
+            this.initUnselectedLinesInputForms(stationsData);
         }
         catch (err) {
             console.warn(err);
+        }
+    }
+    // render the logic for the input form html elements for the lines that are available (available station options, etc)
+    initSelectedLinesInputForms(stationsData) {
+        // iterate through each line object in the data we fetched
+        for (const line in stationsData) {
+            // access the following DOM elements in our template
+            const lineContainer = document.getElementById(`${line}_line_container`);
+            const inputForm = lineContainer?.querySelector("input");
+            const dataList = lineContainer?.querySelector("datalist");
+            // make sure they exist
+            if (inputForm === undefined || inputForm === null || dataList === undefined || dataList === null)
+                throw new Error(`Input form status: ${inputForm}. DataList status: ${dataList}`);
+            // set all the available options for each station
+            stationsData[line]?.forEach((stationObj) => {
+                const option = document.createElement("option");
+                option.setAttribute("value", stationObj.name);
+                option.textContent = stationObj.name;
+                dataList.appendChild(option);
+            });
+            // add additional logic to input form to prevent users for filling answers that don't exist
+            inputForm.addEventListener("change", () => {
+                // if an option was selected or form was filled
+                if (inputForm.value !== "") {
+                    // check if the value put in the input form exists within the datalist
+                    // this is only used in the cases where the user types a value instead of checking an option
+                    let isValidValue = false;
+                    for (const option of dataList.options) {
+                        if (inputForm.value === option.value) {
+                            URLHandler.addQueryParameter("selected_station", inputForm.value);
+                            isValidValue = true;
+                        }
+                    }
+                    if (!isValidValue)
+                        inputForm.value = "";
+                }
+            });
+        }
+    }
+    // remove the line input forms the users didn't select
+    initUnselectedLinesInputForms(stationsData) {
+        // deal with the other subway line containers that aren't in use (since the template has all lines)
+        // get parent container of all lines 
+        const stationsSelectionContainer = document.getElementById("stations_selection_container");
+        if (stationsSelectionContainer === null)
+            throw new Error("The parent stations_selection_container div doesn't exist");
+        // get the div elements holding the lines (organized by color)
+        for (const subwayLineGroups of stationsSelectionContainer.children) {
+            // iterate thru this div element to get each individual line (we're deleting those that are not in use)
+            // make a shallow copy to prevent elements to prevent elements from being skipped since we're deleting elements
+            // unlike python... js/ts actually bases for..of loops based on indices hence the need to track the position
+            for (const subwayLine of [...subwayLineGroups.children]) {
+                if (subwayLine.id[0] !== undefined && !(subwayLine.id[0] in stationsData))
+                    subwayLine.remove();
+            }
         }
     }
     // create the DOM button that actuall triggers redirection to the next page
--- platformpath/platformpathapp/static/platformpathapp/ts/z_stations_selection_page.ts
+++ platformpath/platformpathapp/static/platformpathapp/ts/z_stations_selection_page.ts
@@ -34,73 +34,77 @@ export class StationsSelectionPage {
         const stationsData: {[key: string]: any[]} = await DataFetch.fetchStations(lines, URLS.STATIONS_FETCH_API);
 
         try {
-            const stationsSelectionContainer: HTMLElement | null = document.getElementById("stations_selection_container");
-
-            if (stationsSelectionContainer === null)
-                throw Error("The station selection container div doesn't exist");
-
-            // iterating through each line
-            for (const line in stationsData) { 
-                // create a parent div to house all the forms we'll be creating
-                const formContainer: HTMLElement = document.createElement("div");
-                formContainer.innerHTML = line;
-                // this create a select dropdown form while allowing user typing as well     
-                const datalistForm: HTMLDataListElement = document.createElement("datalist");
-                datalistForm.setAttribute("id", `dropdown_for_${line}`);
-                const inputForm: HTMLInputElement = document.createElement("input");
-                inputForm.setAttribute("list", `dropdown_for_${line}`);
-
-                // save the previous value to remove query params (if the user were to remove them)
-                let previous_value: string = "";
-
-                inputForm.addEventListener("change", () => {
-                    // if an option was selected or form was filled
-                    if (inputForm.value !== "") {
-                        // check if the value put in the input form exists within the datalist
-                        // this is only used in the cases where the user types a value instead of checking an option
-                        let isValidValue: boolean = false;
-
-                        for (const option of datalistForm.options) {
-                            if (inputForm.value === option.value) {
-                                URLHandler.addQueryParameter("selected_station", inputForm.value);
-                                isValidValue = true;
-                            }
-                        }
-
-                        if (!isValidValue) 
-                            inputForm.value = "";
-                    }
-                    else { // if inputForm is empty
-                        //check if query param exists
-                        const allQueryParams: Record<string,string[]> = URLHandler.getQueryParameters();
-                        if (allQueryParams["selected_station"] !== undefined) {
-                            for (const queryParam of allQueryParams["selected_station"]) {
-                                if (queryParam === previous_value) {
-                                    URLHandler.removeQueryParameter("selected_station", previous_value);
-                                    break;
-                                }
-                            }
-                        }
-                    }
-
-                    previous_value = inputForm.value;
-                });
-
-                // create all the dropdown options of available stations
-                stationsData[line]?.forEach((stationObj: any) => {
-                    const option: HTMLElement = document.createElement("option");
-                    option.setAttribute("value", stationObj.name);
-                    option.textContent = stationObj.name;
-                    datalistForm.appendChild(option);
-                });
-
-                // configure all these items so they are grouped up properly in the html
-                formContainer.append(datalistForm, inputForm);
-                stationsSelectionContainer.appendChild(formContainer);
-            }
+            // initiate the logic for the input forms of the lines the users selected
+            this.initSelectedLinesInputForms(stationsData);
+            // initiate the logic for the input forms of the lines the users did not select (remove them basically)
+            this.initUnselectedLinesInputForms(stationsData);
         }
         catch (err: any) {
             console.warn(err);
+        }
+    }
+
+    // render the logic for the input form html elements for the lines that are available (available station options, etc)
+    private initSelectedLinesInputForms(stationsData: {[key: string]: any[]}): void {
+        // iterate through each line object in the data we fetched
+        for (const line in stationsData) {
+            // access the following DOM elements in our template
+            const lineContainer: HTMLElement | null = document.getElementById(`${line}_line_container`);
+            const inputForm: HTMLInputElement | null | undefined = lineContainer?.querySelector("input");
+            const dataList: HTMLDataListElement | null | undefined = lineContainer?.querySelector("datalist");
+
+            // make sure they exist
+            if (inputForm === undefined || inputForm === null || dataList === undefined || dataList === null)
+                throw new Error(`Input form status: ${inputForm}. DataList status: ${dataList}`);
+
+            // set all the available options for each station
+            stationsData[line]?.forEach((stationObj: any) => {
+                const option: HTMLElement = document.createElement("option");
+                option.setAttribute("value", stationObj.name);
+                option.textContent = stationObj.name;
+                dataList.appendChild(option);
+            });
+
+            // add additional logic to input form to prevent users for filling answers that don't exist
+            inputForm.addEventListener("change", () => {
+                // if an option was selected or form was filled
+                if (inputForm.value !== "") {
+                    // check if the value put in the input form exists within the datalist
+                    // this is only used in the cases where the user types a value instead of checking an option
+                    let isValidValue: boolean = false;
+
+                    for (const option of dataList.options) {
+                        if (inputForm.value === option.value) {
+                            URLHandler.addQueryParameter("selected_station", inputForm.value);
+                            isValidValue = true;
+                        }
+                    }
+
+                    if (!isValidValue) 
+                        inputForm.value = "";
+                }
+            });
+        }
+    }
+
+    // remove the line input forms the users didn't select
+    private initUnselectedLinesInputForms(stationsData: {[key: string]: any[]}): void {
+        // deal with the other subway line containers that aren't in use (since the template has all lines)
+        // get parent container of all lines 
+        const stationsSelectionContainer: HTMLElement | null = document.getElementById("stations_selection_container");
+        if (stationsSelectionContainer === null)
+            throw new Error("The parent stations_selection_container div doesn't exist");
+
+        // get the div elements holding the lines (organized by color)
+        for (const subwayLineGroups of stationsSelectionContainer.children) {
+            // iterate thru this div element to get each individual line (we're deleting those that are not in use)
+
+            // make a shallow copy to prevent elements to prevent elements from being skipped since we're deleting elements
+            // unlike python... js/ts actually bases for..of loops based on indices hence the need to track the position
+            for (const subwayLine of [...subwayLineGroups.children]) {
+                if (subwayLine.id[0] !== undefined && !(subwayLine.id[0] in stationsData))
+                    subwayLine.remove();
+            }
         }
     }
 
