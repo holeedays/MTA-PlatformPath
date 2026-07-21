a40d4cd2e6073863477bf1066c892aa0d8f8d3ee
holeedays <weighuang12@gmail.com>
2026-07-21 16:45:33 -0400

    Changed nav bar redirections (dedicated url route for currently unavailable places (assuming that nav bar redirection will lead to a valid page in the future) + refactored views rendering for other 404 errors (doesn't raise Http404 errors now, just shows an empty page with a small heading)

--- platformpath/platformpathapp/views.py
+++ platformpath/platformpathapp/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpRequest, HttpResponse, Http404
+from django.http import HttpRequest, HttpResponse, Http404, HttpResponseNotFound
 
 from .services.slugs import slugify, check_line_slug, check_station_slug
 from .models import Line, Station
@@ -20,23 +20,28 @@ def full_path(request:HttpRequest) -> HttpResponse:
 def lines_selection(request: HttpRequest) -> HttpResponse:
     return render(request, "platformpathapp/lines_selection.html")
 
-def stations_selection(request: HttpRequest, line_slug: str) -> HttpResponse:
+def stations_selection(request: HttpRequest, line_slug: str) -> HttpResponse | HttpResponseNotFound:
+    # check if the line slug is valid
     if (not check_line_slug(line_slug)):
-        raise Http404
+        return HttpResponseNotFound("<h3>Invalid line slug<h3>")
 
     return render(request, "platformpathapp/stations_selection.html")
 
-def interactive_map(request: HttpRequest, line_slug: str, station_slug: str) -> HttpResponse:
+def station_map(request: HttpRequest, line_slug: str, station_slug: str) -> HttpResponse | HttpResponseNotFound:
+    # check if these slugs actually exist within the database
     line_model: Line | None = check_line_slug(line_slug)
     station_model: Station | None = check_station_slug(station_slug)
 
     if (line_model is None or station_model is None):
-        raise Http404
+        return HttpResponseNotFound("<h3>Invalid line and/or station slug<h3>")
     
     # in our slug verifier, we have prefetched the corresponding line and station models already so we can just compare as is
-    # we just have to check if these line and station models are related
+    # make sure to check that even if the line is valid, does belong with this list of stations
     if (not line_model in station_model.lines.all()):
-        raise Http404
+        return HttpResponseNotFound("<h3>Current station does not match with the given subway line<h3>")
     
 
     return render(request, "platformpathapp/station_map.html")
+
+def not_found(request: HttpRequest) -> HttpResponseNotFound:
+    return HttpResponseNotFound("<h3>This page doesn't exist currently, come back later<h3>")
