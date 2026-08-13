8fa605edd73d8665e2bcb70f06afed402c8f074d
Ivan Yeung <ivanyeung0610@gmail.com>
2026-08-13 17:27:53 -0400

    refactor: Update pathfinder for new Edge model
    Update pathfinder to work with the new Edge model which only goes in one
    directional. Additionally, comments were added to make the code in this
    file more understandable/readable.

--- platformpath/platformpathapp/static/platformpathapp/ts/path_finder.ts
+++ platformpath/platformpathapp/static/platformpathapp/ts/path_finder.ts
@@ -1,24 +1,16 @@
-import { type StationResponse, type NodeData, type VerticalDirection } from "./station_data.ts"
-
-export interface RouteTraversal {
-    fromNodeId: number;
-    toNodeId: number;
-    verticalDirection: VerticalDirection;
-}
+import { type StationResponse, type NodeData, type EdgeData } from "./station_data.ts"
 
 export interface PathStep {
     svgId: string;
     layer: string;
     instruction: string;
-    traversal?: RouteTraversal;
+    incomingEdge?: EdgeData; // The edge that leads to this step
 }
 
-interface Neighbor {
-    neighbor: number;
-    instruction: string;
-    verticalDirection: VerticalDirection;
-}
-
+/*
+    This class is used for finding paths between nodes in the station graph
+    given a starting node and an ending node.
+*/
 export class PathFinder {
 
     constructor() {}
@@ -29,16 +21,21 @@ export class PathFinder {
         toNodeId: number,
         isAccessible: boolean = false
     ): PathStep[] | null {
-
+        
+        // Get adjacency map and node maps
+        // The adjacency map shows the neighbors of each node
+        // The node map contains the data for each node in the station
         const adjAndNode = this.getAdjacencyAndNodeMap(station, isAccessible);
-        // check if something went wrong retrieving the adjacency and node maps (should return null)
         if (adjAndNode === null)
             return null;
 
-        const adjacency = adjAndNode[0];
+        const adjacencyMap = adjAndNode[0];
         const nodeMap = adjAndNode[1];
 
         // Build a layer lookup map to translate integer Layer IDs to string svg
+        // This is information is used to indicate which layer of the map to show to the
+        // user for the particular step
+        // Example: database layer 3 -> SVG group "MEZZANINE"
         const layerMap: Record<number, string> = {};
         if (station.layer_models) {
             station.layer_models.forEach(layer => {
@@ -49,50 +46,81 @@ export class PathFinder {
             return null;
         }
 
-        // BFS for pathfinding paths
+        // Breadth-first Search algorithm used to find a path between the two nodes
+        
+        // Keeps track of nodes that have already been visited to prevent loops
         const visited   = new Set<number>();
+        
+        // Get starting node's information to create the first step of the path
         const startNode = nodeMap[fromNodeId];
-
         if (!startNode) {
             console.error(`Start node not found: ${fromNodeId}`);
             return null;
         }
-
         const initialStep: PathStep = {
             svgId:       startNode.svg_id,
             layer:       layerMap[startNode.layer] || "",
             instruction: 'Start here'
         };
 
+        /*
+            This queue stores the next node that should be searched
+
+            Each item contains:
+                - a current node ID
+                - the path to that node as an array of PathSteps
+        */
         const queue: [number, PathStep[]][] = [[fromNodeId, [initialStep]]];
 
+        /*
+            The loop will continue to search for the final node until the
+            destination node has been reached or there are no more nodes to search
+        */
         while (queue.length > 0) {
             const item = queue.shift();
             if (!item) break;
 
             const [currentNodeId, path] = item;
 
-            if (currentNodeId === toNodeId) return path;
-            if (visited.has(currentNodeId)) continue;
+            // The path is returned when is destination node is reached
+            if (currentNodeId === toNodeId) {
+                return path;
+            }
+
+            // If this node is already visited, skip it
+            if (visited.has(currentNodeId)) {
+                continue;
+            }
+
+            // Mark this node as searched
             visited.add(currentNodeId);
 
-            const neighbors = adjacency[currentNodeId] || [];
-            neighbors.forEach(({ neighbor, instruction, verticalDirection }) => {
-                if (!visited.has(neighbor)) {
-                    const targetNode = nodeMap[neighbor];
-                    if (!targetNode) return;
-
-                    queue.push([neighbor, [...path, {
-                        svgId:       targetNode.svg_id,
-                        layer:       layerMap[targetNode.layer] || "",
-                        instruction: instruction,
-                        traversal: {
-                            fromNodeId: currentNodeId,
-                            toNodeId: neighbor,
-                            verticalDirection: verticalDirection,
-                        }
-                    }]]);
+            // Get the outgoing edges for the current node and prepare them to
+            // be searched by creating a new step for them and adding it to the path
+            // Once they are properly prepared, they are added to the queue
+            const outgoingEdges = adjacencyMap[currentNodeId] || [];
+            outgoingEdges.forEach((edge) => {
+                // Edges that have already been searched are not added
+                if (visited.has(edge.to_node)) {
+                    return;
                 }
+
+                const targetNode = nodeMap[edge.to_node];
+                if (!targetNode) {
+                    return;
+                }
+
+                const nextStep: PathStep = {
+                    svgId: targetNode.svg_id,
+                    layer: layerMap[targetNode.layer] || "",
+                    instruction: edge.instruction,
+                    incomingEdge: edge,
+                };
+
+                queue.push([
+                    edge.to_node,
+                    [...path, nextStep],
+                ]);
             });
         }
 
@@ -100,21 +128,26 @@ export class PathFinder {
         return null;
     }
 
-    // get adjacency and Nodemap
+    // A helper function to build the adjacency map and node map
+    // Adjacencey map: A dictionary mapping each node to its outgoing edges
+    // Node map: A dictionary mapping each node ID to its corresponding node data
     private getAdjacencyAndNodeMap(
         station: StationResponse,
         isAccessible: boolean = false
-    ): [Record<number, Neighbor[]>, Record<number, NodeData>] | null {
-          // Build node lookup map
+    ): [Record<number, EdgeData[]>, Record<number, NodeData>] | null {
+        
+        // Build node lookup map by looping through every node in the station
         const nodeMap: Record<number, NodeData> = {};
         station.node_models.forEach(node => {
             nodeMap[node.id] = node;
         });
 
-        // Build adjacency map
-        const adjacency: Record<number, Neighbor[]> = {};
+        // Build adjacency map by looping thorugh every edge in the station
+        // and organizing them based on the fromNode of the edge
+        const adjacency: Record<number, EdgeData[]> = {};
 
         station.edge_models.forEach((edge) => {
+            // inactive endges are will not be considered for the route
             if (!edge.is_active) return null;
 
             const fromNode = nodeMap[edge.from_node];
@@ -128,38 +161,12 @@ export class PathFinder {
             if (isAccessible && this.isNotAccessible(fromNode)) return;
             if (isAccessible && this.isNotAccessible(toNode))   return;
 
-            // addressing for reverese directions (a -> b, b -> a)
-            const fromNeighbors = adjacency[edge.from_node] ?? [];
-            const toNeighbors = adjacency[edge.to_node] ?? [];
-            adjacency[edge.from_node] = fromNeighbors;
-            adjacency[edge.to_node] = toNeighbors;
-
-            fromNeighbors.push({
-                neighbor:    edge.to_node,
-                instruction: edge.instruction_forward,
-                verticalDirection: edge.forward_vertical_direction
-            });
-            toNeighbors.push({
-                neighbor:    edge.from_node,
-                instruction: edge.instruction_backward,
-                verticalDirection: this.reverseDirection(edge.forward_vertical_direction)
-            });
+            const outgoingEdges = adjacency[edge.from_node] ?? [];
+            outgoingEdges.push(edge);
+            adjacency[edge.from_node] = outgoingEdges;
         });
 
         return [adjacency, nodeMap];
-    }
-    
-    // Helper function for getting the direction of the stairs
-    private reverseDirection(direction: VerticalDirection): VerticalDirection {
-    switch (direction) {
-        case "UP":
-            return "DOWN";
-        case "DOWN":
-            return "UP";
-        default:
-            return "NONE";
-    }
-    
     }
 
     // method to determine if a node is not accessible
